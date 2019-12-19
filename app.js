c3e4341a6b06fa800cba03241178284eb33a5dae
Steven Chang <1632460272@qq.com>
2019-12-19 22:21:44 +0000

    Implementing some basic functions

--- app.js
+++ app.js
@@ -9,11 +9,29 @@ GAME RULES:
 
 */
 
-var scores, currentScore, currentPlayer, dice;
+var scores, currentScore, currentPlayer;
 
 scores = [0,0];
 currentScore = 0;
-currentScore = 0;
-dice = Math.floor(Math.random() * 6) + 1;
+currentPlayer = 0;
 
-document.querySelector("#score-0");
+document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
+document.querySelector(".btn-roll").addEventListener("click", function(){
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    var diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-"+dice+".png";
+
+    if (dice != 1){
+        document.querySelector("#current-" + currentPlayer).textContent = dice;
+    }else{
+        document.querySelector("#current-" + currentPlayer).textContent = 0;
+    }
+});
